81015d082e61a7869325f579748673e91140cbe6
Bedirhan Deveci <bedirhanyigitdeveci@gmail.com>
2026-04-11 13:19:56 +0300

    configurations

---
+++ src/main/java/com/example/library/management/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.example.library.management.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Apply to all your API endpoints
+                        .allowedOrigins("http://localhost:5173") // Your exact React URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP actions
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
